0eee883e0078a10e05c6c6e27f9da45239772938
isaakamerigo <landen_g@yahoo.com>
2021-07-01 19:34:57 -0400

    navbar & route in app.js

--- guardian/src/App.js
+++ guardian/src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
+import { Route } from 'react-dom';
+// import axios from 'axios';
+import Navbar from './component/Navbar';
+// import Knowledge from './component/Knowledge';
+// import Tool from './component/Tool';
 import './App.css';
 
+
 function App() {
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+<Navbar />
+      
+        <Route exact path="/">
+        
+
+      </Route>
+
+        <Route exact path="/newKnow">
+    
+
+      </Route>
+
+        <Route exact path="/newPur">
+      
+      </Route>
+
+        <Route exact path="/newTool">
+      
+      </Route>
+
     </div>
   );
 }
---
+++ guardian/src/component/Navbar.jsx
@@ -0,0 +1,16 @@
+import { Link } from 'react-router-dom';
+
+function Navbar() {
+
+  return(
+<nav>
+  <Link to="/">Guardian</Link>
+  <Link to="/newKnow">Knowledge</Link>
+  <Link to="/newPur">Purpose</Link>
+  <Link to="/newTool">Tool</Link>
+</nav>
+
+  )
+}
+
+export default Navbar;
