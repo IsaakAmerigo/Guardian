42cee884394bded76b038f8df87b88635c217fea
isaakamerigo <landen_g@yahoo.com>
2021-07-07 12:30:20 -0400

    update

--- guardian/src/App.js
+++ guardian/src/App.js
@@ -37,7 +37,7 @@ function App() {
   <h2>Welcome to Guardian</h2>
 <img src="https://i.imgur.com/MauQhiA.jpg" alt="roman god mars" id="mars"></img>
 <p>
-<em> Image from myancientworld.tumblr.com</em>
+{/* Image from myancientworld.tumblr.com */}
 </p>
   <p>
     You've been lead here via a divine calling by the <strong id="chad">Triad of Masculinity</strong>, by the Roman God, Mars. Called by your Will to Power; by a desire to physically defense community, family & Fatherland as we cultural play offensive to Save the West. So check on the link to our Purpose up top then have it from there.<br/><strong id="chad">LET'S GOOOOOO</strong>
